0b8a41c283723e59fc6a15f6ec7d0e4003b1bda5
Nayab-eng-creator <nayab.goher786@gmail.com>
2024-10-25 11:18:05 -0700

    Add files via upload

--- mycode.asm
+++ mycode.asm
@@ -1,82 +1,41 @@
-.model small
-.stack 100h
-.data
-msg db 'entre baes= $'
-msg1 db 'entre height= $'
-msg2 db 'result= $'
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here
+ .model small
+ .stack 100h
+ .data
+ .code
+ main proc
+ mov dl,'i' 
+ mov ah,2
+ int 21h
+ mov dl,'s'
+ mov ah,2
+ int 21h
+ mov ah,4ch
+ int 21h
+ end main p  
+ main end
 
 
-.code    
-main proc
-    mov ax,@data
-    mov ds,ax
-    mov dl,msg
-    mov dx,offset msg
-    mov ah,9
-    int 21h
-    
-    mov ah,1
-    int 21h
-    mov bl,al
-    sub bl,48
-    mov dl,10
-    mov ah,2
-    int 21h
-    
-        mov ax,@data
-    mov ds,ax
-    mov dl,msg1
-    mov dx,offset msg1
-    mov ah,9
-    int 21h 
-             
-    mov ah,1
-    int 21h
-    mov cl,al
-    sub cl,48
-    mov dl,10
-    mov ah,2
-    int 21h
-             
-    mov dl,13
-    mov ah,2
-    int 21h
-    mov dl,13
-    mov ah,2
-    int 21h  
-    
-    mov ax,@data
-    mov ds,ax
-    mov dl,msg2
-    mov dx,offset msg2
-    mov ah,9
-    int 21h   
-    
-    mov al,bl
-    mov bl,2 
-    mul cl
-    div bl
-    mov dl,al
-    add dl,48
-    
-    AAM
-    
-   mov ch,ah
-   mov cl,al
-   
-   mov dl,ch
-   add dl,48
-   mov ah,2
-   int 21h
-   mov dl,cl
-   add dl,48
-   mov ah,2
-   int 21h
-    
-    
-   
-    
-    mov ah, 4ch
-    int 21h
-main endp
-end main
+
+ret
+
+
+
+
+
+ret
+
+
+
+
